359cbdc18c2766b7c29c356fba88300eeff72761
huangwei <1142488172@qq.com>
2024-03-12 18:04:49 +0800

    新增动态公共配置示例

---
+++ java_maven_profile/java_maven_group_01/src/main/java/org/example/App.java
@@ -0,0 +1,19 @@
+package org.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Hello world!
+ *
+ */
+@SpringBootApplication
+
+public class App
+{
+    public static void main( String[] args )
+    {
+
+        SpringApplication.run(App.class);
+    }
+}
